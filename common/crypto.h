005b708510999e78889d5f967be60a9d1b66e35c
Vital Schonkeren <vital.schonkeren@be.zetes.com>
2020-03-31 21:12:51 +0200

    added signing verifier

--- common/crypto.h
+++ common/crypto.h
@@ -1,16 +1,15 @@
 #ifndef ___crypto_h____
 #define ___crypto_h____
 
-//#include "platform.hpp"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-   int verifyRSASignature(unsigned char *plain, unsigned int l_plain, const unsigned char* cert, int l_cert, unsigned char *signature, unsigned l_signature);
-   int verifyRSASignature2(unsigned char *plain, unsigned int l_plain, const unsigned char* cert, int l_cert, unsigned char *signature, unsigned l_signature);
+int verifyRSASignature(unsigned char *plain, unsigned int l_plain, const unsigned char* cert, int l_cert, unsigned char *signature, unsigned l_signature);
+int verifyRSASignature2(unsigned char *plain, unsigned int l_plain, const unsigned char* cert, int l_cert, unsigned char *signature, unsigned l_signature);
 
 #ifdef __cplusplus
+
 }
 #endif
 
