77e79b8eb96a8d33f315b62952da635d86279ee4
Vital Schonkeren <vital.schonkeren@be.zetes.com>
2020-05-11 17:58:35 +0200

    added some testcode

---
+++ common/test.hpp
@@ -0,0 +1,9 @@
+#ifndef __RUN_TEST_H__
+#define __RUN_TEST_H__
+                                         
+
+//int ParseCL(int argc, char *argv[], COMMAND_PARAM *pCommand);
+int runTest(int argc, const char * argv[]);
+
+#endif //__RUN_TEST_H__
+
