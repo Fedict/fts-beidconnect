0cc2e3072cb80812175f094d9ac476560fbba87d
Christophe Peerens <c.peerens@neomytic.be>
2022-07-15 12:48:50 +0200

    change activeX version

--- ActiveX/make.bat
+++ ActiveX/make.bat
@@ -1,8 +1,34 @@
-@if not "%WindowsSdkDir%" == ""  goto build
+@echo off
+if not "%WindowsSdkDir%" == "" goto build
 
-@call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
+echo Setup Env...
 
-@set PATH=%WindowsSdkDir%bin\%WindowsSDKVersion%\x86;%PATH%
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\" (
+    echo Found 2022 Professional
+    call "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat"
+    goto build
+)
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\" (
+    echo Found 2022 Community
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+    goto build
+)
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\" (
+    echo Found 2019 Professional
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+    goto build
+)
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\" (
+    echo Found 2019 Community
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+    goto build
+)
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\" (
+    echo Found 2019 BuildTools
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
+    goto build
+)
 
 :build
+echo Start Build...
 nmake %1
